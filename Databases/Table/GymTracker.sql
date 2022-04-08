1b8f3986d4c2c653f3175f411a68b9404bae94b7
Kelvin789 <kdjace21@gmail.com>
2022-04-08 23:43:46 +0100

    Database sql file removed as full script already existed.

--- Databases/Table/GymTracker.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [Gym].[dbo].[GymTracker1] (
-	[ID] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
-	[DateCreated] DATETIME NOT NULL,
-	[BodyPart] NVARCHAR(50) NULL,
-	[Exercise] NVARCHAR(50) NULL,
-	[Sets] NVARCHAR(50) NULL,
-	[Reps] NVARCHAR(50) NULL,
-	[Weights] NVARCHAR(50) NULL
-)
